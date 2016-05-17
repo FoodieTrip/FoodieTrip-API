2b99efbdee499b307a28cd693790fca7e800f61a
Lear the Whiteash <Whiteash212@users.noreply.github.com>
2016-05-17 11:17:05 +0800

    修正連線放置位置
    將連線部分的code放到各個功能內, 有啟用的時候才和DB連線

--- routes/group.js
+++ routes/group.js
@@ -1,28 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var r = require('rethinkdbdash')({
-    port: 28015,
-    host: 'localhost',
-    db: 'FoodieTrip'
-});
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-    //res.send('ID is: ' + req.params.id + ', shop name is: 熊大拉麵');
+    
+    var r = require('rethinkdbdash')({
+        port: 28015,
+        host: 'localhost',
+        db: 'FoodieTrip'
+    });
+
     var id = parseInt(req.params.id);
     r.table("Group").get(id)
-  .run()
-  .then(function (response) {
+   .run()
+   .then(function (response) {
       res.send(response);
       console.log(response);
-  })
-  .error(function (err) {
+   })
+   .error(function (err) {
       console.log(err);
-  })
+   })
   
 });
 
 router.get('/', function (req, res, next) {
+    var r = require('rethinkdbdash')({
+        port: 28015,
+        host: 'localhost',
+        db: 'FoodieTrip'
+    });
+
     r.table("Group")
     .run()
     .then(function (response) {
