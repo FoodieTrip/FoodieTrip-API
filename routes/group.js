ceaf062fd159904e25b608000ed0ffe7184c9f4d
Lear the Whiteash <Whiteash212@users.noreply.github.com>
2016-05-17 11:07:52 +0800

    寫好查詢所有團體 & 查詢特地團體的功能
    如題

--- routes/group.js
+++ routes/group.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var r = require('rethinkdbdash')({
+    port: 28015,
+    host: 'localhost',
+    db: 'FoodieTrip'
+});
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  res.send('ID is: ' + req.params.id + ', shop name is: 熊大拉麵');
-  
-  //res.render('index', { title: '889' , name: req.query.name});
-  //if(req.accepts('json')) blablabla
+    //res.send('ID is: ' + req.params.id + ', shop name is: 熊大拉麵');
+    var id = parseInt(req.params.id);
+    r.table("Group").get(id)
+  .run()
+  .then(function (response) {
+      res.send(response);
+      console.log(response);
+  })
+  .error(function (err) {
+      console.log(err);
+  })
   
 });
 
-router.get('/', function(req, res, next) {
-  res.send('Shop Name: 熊大拉麵');
-});
+router.get('/', function (req, res, next) {
+    r.table("Group")
+    .run()
+    .then(function (response) {
+        res.send(response);
+        console.log(response);
+    })
+    .error(function (err) {
+        console.log(err);
+    })
+
+    });
 module.exports = router;
