22fab56b13051deb3934e2d8157f1500c576fee1
Lear the Whiteash <Whiteash212@users.noreply.github.com>
2016-05-31 17:09:52 +0800

    Add files via upload

--- routes/group.js
+++ routes/group.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET sepcific group inform */
 router.get('/:id', function(req, res, next) {
     
     var r = require('rethinkdbdash')({
@@ -23,6 +23,33 @@ router.get('/:id', function(req, res, next) {
   
 });
 
+/* Insert into db : Join a group */
+router.post('/:id', function(req, res, next) {
+    
+    var r = require('rethinkdbdash')({
+        port: 28015,
+        host: 'localhost',
+        db: 'FoodieTrip'
+    });
+
+    var id = parseInt(req.params.id);
+	var order_name = req.query.order_name;
+	var order = req.query.order;
+	var num = req.query.num;
+	
+	r.table("Order").insert({g_id: id, order_name: order_name, order: order, num: num})
+   .run( function(err, result) {
+		if (err) throw err;
+			console.log(JSON.stringify(result, null, 2));
+    })
+	.then(function (response) {
+        res.send("Insert succed!");
+        console.log("Inserted.");
+    })
+});
+
+
+/* GET all group list */
 router.get('/', function (req, res, next) {
     var r = require('rethinkdbdash')({
         port: 28015,
@@ -41,4 +68,34 @@ router.get('/', function (req, res, next) {
     })
 
     });
+
+/* Create a group*/
+router.post('/', function(req, res, next) {
+    
+    var r = require('rethinkdbdash')({
+        port: 28015,
+        host: 'localhost',
+        db: 'FoodieTrip'
+    });
+
+	var gname = req.query.gname;
+	var sname = req.query.sname;
+	var gopentime = req.query.gopentime;
+	var gclosetime = req.query.gclosetime;
+	var amount = req.query.amount;
+	var place = req.query.place;
+	var arrivetime = req.query.arrivetime;
+	var menu = req.query.menu;
+	
+	r.table("Group").insert({gname: gname, sname: sname, gopentime: gopentime, gclosetime:gclosetime, amount:amount, place:place, arrivetime:arrivetime, menu:menu})
+   .run( function(err, result) {
+		if (err) throw err;
+			console.log(JSON.stringify(result, null, 2));
+    })
+	.then(function (response) {
+        res.send("Insert succed!");
+        console.log("Inserted.");
+    })
+});
+	
 module.exports = router;
