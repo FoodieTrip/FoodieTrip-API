56a880ea0b29481c2613ae98c11c845e221333be
Lear the Whiteash <Whiteash212@users.noreply.github.com>
2016-06-03 17:09:41 +0800

    implement insert of group & shop

--- routes/group.js
+++ routes/group.js
@@ -77,7 +77,7 @@ router.post('/', function(req, res, next) {
         host: 'localhost',
         db: 'FoodieTrip'
     });
-
+/*
 	var gname = req.query.gname;
 	var sname = req.query.sname;
 	var gopentime = req.query.gopentime;
@@ -87,106 +87,18 @@ router.post('/', function(req, res, next) {
 	var arrivetime = req.query.arrivetime;
 	var menu = req.query.menu;
 	
-	r.table("Group").insert({gname: gname, sname: sname, gopentime: gopentime, gclosetime:gclosetime, amount:amount, place:place, arrivetime:arrivetime, menu:menu})
-   .run( function(err, result) {
-		if (err) throw err;
-			console.log(JSON.stringify(result, null, 2));
-    })
-	.then(function (response) {
-        res.send("Insert succed!");
-        console.log("Inserted.");
-    })
-});
+	console.log(req.query);*/
 	
-module.exports = router;
-var express = require('express');
-var router = express.Router();
-
-/* GET sepcific group inform */
-router.get('/:id', function(req, res, next) {
-    
-    var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-
-    var id = parseInt(req.params.id);
-    r.table("Group").get(id)
-   .run()
-   .then(function (response) {
-      res.send(response);
-      console.log(response);
-   })
-   .error(function (err) {
-      console.log(err);
-   })
-  
-});
-
-/* Insert into db : Join a group */
-router.post('/:id', function(req, res, next) {
-    
-    var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-
-    var id = parseInt(req.params.id);
-	var order_name = req.query.order_name;
-	var order = req.query.order;
-	var num = req.query.num;
 	
-	r.table("Order").insert({g_id: id, order_name: order_name, order: order, num: num})
-   .run( function(err, result) {
-		if (err) throw err;
-			console.log(JSON.stringify(result, null, 2));
-    })
-	.then(function (response) {
-        res.send("Insert succed!");
-        console.log("Inserted.");
-    })
-});
-
-
-/* GET all group list */
-router.get('/', function (req, res, next) {
-    var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-
-    r.table("Group")
-    .run()
-    .then(function (response) {
-        res.send(response);
-        console.log(response);
-    })
-    .error(function (err) {
-        console.log(err);
-    })
-
-    });
-
-/* Create a group*/
-router.post('/', function(req, res, next) {
-    
-    var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-
-	var gname = req.query.gname;
-	var sname = req.query.sname;
-	var gopentime = req.query.gopentime;
-	var gclosetime = req.query.gclosetime;
-	var amount = req.query.amount;
-	var place = req.query.place;
-	var arrivetime = req.query.arrivetime;
-	var menu = req.query.menu;
+	var gname = req.body.gname;
+	var sname = req.body.sname;
+	var gopentime = req.body.gopentime;
+	var gclosetime = req.body.gclosetime;
+	var amount = req.body.amount;
+	var place = req.body.place;
+	var arrivetime = req.body.arrivetime;
+	var menu = req.body.menu;
+	
 	
 	r.table("Group").insert({gname: gname, sname: sname, gopentime: gopentime, gclosetime:gclosetime, amount:amount, place:place, arrivetime:arrivetime, menu:menu})
    .run( function(err, result) {
@@ -194,8 +106,8 @@ router.post('/', function(req, res, next) {
 			console.log(JSON.stringify(result, null, 2));
     })
 	.then(function (response) {
-        res.send("Insert succed!");
-        console.log("Inserted.");
+        res.send(req.body);
+        console.log(req.body);
     })
 });
 	
--- routes/shop.js
+++ routes/shop.js
@@ -48,95 +48,42 @@ router.post('/', function(req, res, next) {
         host: 'localhost',
         db: 'FoodieTrip'
     });
-
-	var gname = req.query.gname;
+/*
+	var s_id = req.query.s_id;
 	var sname = req.query.sname;
-	var gopentime = req.query.gopentime;
-	var gclosetime = req.query.gclosetime;
-	var amount = req.query.amount;
-	var place = req.query.place;
-	var arrivetime = req.query.arrivetime;
+	var tel = req.query.tel;
+	var addr = req.query.addr;
+	var pic = req.query.pic;
 	var menu = req.query.menu;
+	var sopentime = req.query.sopentime;
+	var sclosetime = req.query.sclosetime;
+	var price_low = req.query.price_low;
+	var price_high = req.query.price_high;
+	*/
+	var s_id = req.body.s_id;
+	var sname = req.body.sname;
+	var tel = req.body.tel;
+	var addr = req.body.addr;
+	var pic = req.body.pic;
+	var menu = req.body.menu;
+	var sopentime = req.body.sopentime;
+	var sclosetime = req.body.sclosetime;
+	var price_low = req.body.price_low;
+	var price_high = req.body.price_high;
+	/*
+	console.log("Post is: "+req.post);
+	res.send("Post is: " +req.post);
+	*/
 	
-	r.table("Shop").insert({gname: gname, sname: sname, gopentime: gopentime, gclosetime:gclosetime, amount:amount, place:place, arrivetime:arrivetime, menu:menu})
+	
+	r.table("Shop").insert({s_id: s_id, sname: sname, tel:tel, addr:addr, pic:pic, menu:menu, sopentime:sopentime, sclosetime:sclosetime, price_low:price_low, price_high:price_high})
    .run( function(err, result) {
 		if (err) throw err;
 			console.log(JSON.stringify(result, null, 2));
     })
 	.then(function (response) {
-        res.send("Insert succed!");
-        console.log("Inserted.");
-    })
-});
-module.exports = router;
-var express = require('express');
-var router = express.Router();
-
-
-/* GET users listing. */
-router.get('/:id', function(req, res, next) {
-	var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-	
-    var id = parseInt(req.params.id);
-    r.table("Shop").get(id)
-	.run()
-	.then(function (response) {
-	  res.send(response);
-	  console.log(response);
-	})
-	.error(function (err) {
-	  console.log(err);
-	})
-});
-
-router.get('/', function (req, res, next) {
-	var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-	
-    r.table("Shop")
-    .run()
-    .then(function (response) {
-        res.send(response);
-        console.log(response);
-    })
-    .error(function (err) {
-        console.log(err);
-    })
-});
-
-/* Create a Shop*/
-router.post('/', function(req, res, next) {
-    
-    var r = require('rethinkdbdash')({
-        port: 28015,
-        host: 'localhost',
-        db: 'FoodieTrip'
-    });
-
-	var gname = req.query.gname;
-	var sname = req.query.sname;
-	var gopentime = req.query.gopentime;
-	var gclosetime = req.query.gclosetime;
-	var amount = req.query.amount;
-	var place = req.query.place;
-	var arrivetime = req.query.arrivetime;
-	var menu = req.query.menu;
-	
-	r.table("Shop").insert({gname: gname, sname: sname, gopentime: gopentime, gclosetime:gclosetime, amount:amount, place:place, arrivetime:arrivetime, menu:menu})
-   .run( function(err, result) {
-		if (err) throw err;
-			console.log(JSON.stringify(result, null, 2));
-    })
-	.then(function (response) {
-        res.send("Insert succed!");
-        console.log("Inserted.");
+        res.send(req.body);
+        console.log(req.body);
     })
 });
 module.exports = router;
